b5e6a86b7d893b9cce268c22ec1c044033ece578
sonto <sonto@gmail.com>
2024-06-30 20:42:52 -0500

    uh

---
+++ battle_new.py
@@ -0,0 +1,6 @@
+from char import stats
+from char import name
+from enemies import current_enemy
+import time
+import random
+# basically required imports. maybe could be made from scratch but i doubt it 
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-player_name = input("enter a name:")
+import time
+import char
+import enemies
+player_name = input('''
+What is your name?
+>''')
 
 print(f"Hello, {player_name}!")
+time.sleep(1)
+print("Welcome to RPG!!")
+
+import battle
 
