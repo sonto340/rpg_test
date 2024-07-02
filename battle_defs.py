2cd4e60f431aed5bf857aacbc2eb61c520a2a379
Joseph <sonto340@gmail.com>
2024-07-02 14:28:13 -0500

    cant catcha break

--- battle_defs.py
+++ battle_defs.py
@@ -2,11 +2,21 @@ dmg = 47
 from enemies import current_enemy
 def enemy_turn(damage):
     print(f'''It's {current_enemy["name"]}'s turn!''')
-    print(f'''{current_enemy["name"]} did {damage} damage''')
-    stats["hp"] =- dmg 
-    
-    if stats["hp"] <= 0:
-        player_alive = False
+    if (ehit_rate * ran_num[x]) > (stats["acc"] / ran_num[x]) 
+        print("Oh no, a hit!")
+        print(f'''{current_enemy["name"]} did {damage} damage''')
+        stats["hp"] =- dmg 
+        if stats["hp"] <= 0:
+            player_alive = False
+        else:
+            print("Time to striek back!")
+    else:
+        print("Aw yeah, they whiffed it!")
+        break
     else:
         print("Player turn message")
         break
+
+
+def player_turn(damage):
+
